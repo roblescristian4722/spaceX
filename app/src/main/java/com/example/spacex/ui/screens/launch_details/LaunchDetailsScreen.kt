83c5f0878f18dd958d8e36a5bdc246c0358bc2ff
roblescristian4722 <roblescristian4722@gmail.com>
2024-10-18 10:17:07 -0600

    Removing not-used imports

--- app/src/main/java/com/example/spacex/ui/screens/launch_details/LaunchDetailsScreen.kt
+++ app/src/main/java/com/example/spacex/ui/screens/launch_details/LaunchDetailsScreen.kt
@@ -3,12 +3,9 @@ package com.example.spacex.ui.screens.launch_details
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.spacex.utils.Alert
-import org.koin.androidx.viewmodel.ext.android.viewModel
-import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun LaunchDetailsScreen(navController: NavController) {
