6419a557baacd158fe2f0f4a77e887a30badc63a
roblescristian4722 <roblescristian4722@gmail.com>
2024-10-22 11:21:39 -0600

    Adding portrait mode list

--- app/src/main/java/com/example/spacex/ui/screens/launch_list/LaunchListScreen.kt
+++ app/src/main/java/com/example/spacex/ui/screens/launch_list/LaunchListScreen.kt
@@ -1,5 +1,6 @@
 package com.example.spacex.ui.screens.launch_list
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,6 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,6 +31,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -113,12 +118,20 @@ fun ComposableView(navController: NavController, items: List<LaunchesEntity>,
                             ))
                         }
                     ) { innerPadding ->
-                        LazyColumn (
-                            Modifier.padding(innerPadding),
-                            verticalArrangement = Arrangement.spacedBy(10.dp)
-                        ) {
-                            items(items) { item ->
-                                LaunchListItemScreen(navController, item)
+                        if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                            LazyVerticalGrid(modifier = Modifier.padding(innerPadding),
+                                columns = GridCells.Adaptive(minSize = 300.dp)) {
+                                items(items) { item ->
+                                    LaunchListItemScreen(navController, item)
+                                }
+                            }
+                        } else {
+                            LazyColumn (modifier = Modifier.padding(innerPadding),
+                                verticalArrangement = Arrangement.spacedBy(10.dp)
+                            ) {
+                                items(items) { item ->
+                                    LaunchListItemScreen(navController, item)
+                                }
                             }
                         }
                     }
